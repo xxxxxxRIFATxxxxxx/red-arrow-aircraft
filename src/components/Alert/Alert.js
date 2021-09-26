f8ff8f02d33376172f9cb370b7072b19e4ccdf4b
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-09-26 20:08:24 +0600

    Alert Function Added

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alert = () => {
+    return (
+        <div className="alert alert-danger" role="alert">
+            It is already added in the team
+        </div>
+    );
+};
+
+export default Alert;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import Person from '../Person/Person';
 import Team from '../Team/Team';
+import Alert from '../Alert/Alert';
 
 const Persons = () => {
     const [persons, setPersons] = useState([]);
     const [team, setTeam] = useState([]);
+    const [alert, setAlert] = useState(false);
 
     const handleSelectPerson = person => {
-        const newTeam = [...team, person];
-        setTeam(newTeam);
+        const personFound = team.find(item => item.id === person.id);
+        if (personFound) {
+            setAlert(true);
+
+            setTimeout(() => {
+                setAlert(false);
+            }, 2000);
+        }
+
+        else {
+            const newTeam = [...team, person];
+            setTeam(newTeam);
+        };
     };
 
     useEffect(() => {
@@ -39,6 +52,7 @@ const Persons = () => {
                 </div>
 
                 <div className="col-12 col-sm-12 col-md-3">
+                    {alert ? <Alert></Alert> : null}
                     <Team team={team}></Team>
                 </div>
             </div>
